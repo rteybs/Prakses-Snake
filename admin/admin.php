620c95a84651e1eaf3441283e1a95008e8117923
Toms Haritonovs <haritonovstoms2008@Gmail.com>
2026-05-23 17:54:03 +0300

    izveidoja stilu priekš admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrācija</title>
-    <link rel="stylesheet" href="../css/OneBoxStyle.css">
+    <link rel="stylesheet" href="../css/AdminStyle.css">
 </head>
 <body>
 <form class="container">
